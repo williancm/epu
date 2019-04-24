9d4b6562623ef0f0441ae0089390dfcd174ed2ea
Willian Matheus Marini <willianmatheuscm001@gmail.com>
2019-04-23 22:31:18 -0300

    Local host Ok, Problemas com Json

--- src/main/java/br/edu/epualt/resource/ResponsavelResource.java
+++ src/main/java/br/edu/epualt/resource/ResponsavelResource.java
@@ -4,9 +4,7 @@ import br.edu.epualt.domain.Responsavel;
 
 import br.edu.epualt.repository.ResponsavelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
